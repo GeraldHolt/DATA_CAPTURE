3b530387ad9eec06c67c67d9cc84c2ad9a7e1432
GeraldHolt <64152435+GeraldHolt@users.noreply.github.com>
2024-11-26 08:40:22 +0200

    Initial Push

--- Home.py
+++ Home.py
@@ -7,8 +7,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-from st_aggrid import AgGrid, GridOptionsBuilder, DataReturnMode
-from st_aggrid import GridUpdateMode
+
 
 from pages import *
 
